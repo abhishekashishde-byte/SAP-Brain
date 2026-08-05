032eec925354e4b2ea42013452c631db0e0da4d1
abhishekashishde-byte <abhishek.ashish.de@gmail.com>
2026-08-05 18:20:57 +0200

    Install authenticated API fetch wrapper

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { installAuthenticatedFetch } from './apiFetchAuth.js'
+
+installAuthenticatedFetch()
+
 ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
