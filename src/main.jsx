99832b5353723773024411cf68cf83f0536e1af1
abhishekashishde-byte <abhishek.ashish.de@gmail.com>
2026-08-05 22:29:04 +0200

    Install quota visual guard

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import './index.css'
 import App from './App.jsx'
 import { installAuthenticatedFetch } from './apiFetchAuth.js'
 import { installCreditFab } from './creditFab.js'
+import { installQuotaVisualGuard } from './quotaVisualGuard.js'
 
 installAuthenticatedFetch()
 
 ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
 
 installCreditFab()
+installQuotaVisualGuard()
